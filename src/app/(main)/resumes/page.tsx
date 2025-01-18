d9aa10e4fab423e43d2e3cb4f17316c360953ef2
vinrain-coder <vincentombogo57@gmail.com>
2025-01-18 08:48:32 +0000

    03-add next themes

---
+++ src/app/(main)/resumes/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <main>Here will be your resumes</main>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,7 +11,8 @@ export const metadata: Metadata = {
     template: "%s - AI Resume Builder",
     absolute: "AI Resume Builder",
   },
-  description: "AI resume builder is the easiest way to build a professional resume",
+  description:
+    "AI resume builder is the easiest way to build a professional resume",
 };
 
 export default function RootLayout({
@@ -18,8 +21,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }

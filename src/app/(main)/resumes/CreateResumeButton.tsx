024ef89eb4c9235e6983fb8cabe607d3d7958917
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-02-06 10:48:09 +0000

    add editor button

--- src/app/(main)/resumes/CreateResumeButton.tsx
+++ src/app/(main)/resumes/CreateResumeButton.tsx
@@ -1,37 +1,16 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-// import usePremiumModal from "@/hooks/usePremiumModal";
 import { PlusSquare } from "lucide-react";
 import Link from "next/link";
 
-interface CreateResumeButtonProps {
-  canCreate: boolean;
-}
-
-export default function CreateResumeButton({
-  canCreate,
-}: CreateResumeButtonProps) {
-  // const premiumModal = usePremiumModal();
-
-  if (canCreate) {
-    return (
-      <Button asChild className="mx-auto flex w-fit gap-2">
-        <Link href="/editor">
-          <PlusSquare className="size-5" />
-          New resume
-        </Link>
-      </Button>
-    );
-  }
-
+export default function CreateResumeButton() {
   return (
-    <Button
-      // onClick={() => premiumModal.setOpen(true)}
-      className="mx-auto flex w-fit gap-2"
-    >
-      <PlusSquare className="size-5" />
-      New resume
+    <Button asChild className="mx-auto flex w-fit gap-2">
+      <Link href="/editor">
+        <PlusSquare className="size-5" />
+        New resume
+      </Link>
     </Button>
   );
 }

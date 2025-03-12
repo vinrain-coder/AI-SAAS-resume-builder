b4a321f7704c949f506a6a939134ba08aba39d88
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-03-12 18:50:07 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "w0mlmrgwbziwquaq.public.blob.vercel-storage.com",
+        hostname: "*.blob.vercel-storage.com",
       },
     ],
   },

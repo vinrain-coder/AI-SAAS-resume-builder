862fd4a93397ef3a76325b52c454a58f5dce2d5b
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-03-12 14:10:41 +0000

    Fix openai

--- src/app/(main)/editor/forms/actions.ts
+++ src/app/(main)/editor/forms/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import openai from "@/lib/openai";
+// import openai from "@/lib/openai";
 // import { canUseAITools } from "@/lib/permissions";
 // import { getUserSubscriptionLevel } from "@/lib/subscription";
 import {
@@ -70,27 +70,27 @@ export async function generateSummary(input: GenerateSummaryInput) {
   console.log("systemMessage", systemMessage);
   console.log("userMessage", userMessage);
 
-  const completion = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    messages: [
-      {
-        role: "system",
-        content: systemMessage,
-      },
-      {
-        role: "user",
-        content: userMessage,
-      },
-    ],
-  });
+  // const completion = await openai.chat.completions.create({
+  //   model: "gpt-4o-mini",
+  //   messages: [
+  //     {
+  //       role: "system",
+  //       content: systemMessage,
+  //     },
+  //     {
+  //       role: "user",
+  //       content: userMessage,
+  //     },
+  //   ],
+  // });
 
-  const aiResponse = completion.choices[0].message.content;
+  // const aiResponse = completion.choices[0].message.content;
 
-  if (!aiResponse) {
-    throw new Error("Failed to generate AI response");
-  }
+  // if (!aiResponse) {
+  //   throw new Error("Failed to generate AI response");
+  // }
 
-  return aiResponse;
+  // return aiResponse;
 }
 
 export async function generateWorkExperience(
@@ -126,33 +126,33 @@ export async function generateWorkExperience(
   ${description}
   `;
 
-  const completion = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    messages: [
-      {
-        role: "system",
-        content: systemMessage,
-      },
-      {
-        role: "user",
-        content: userMessage,
-      },
-    ],
-  });
+  // const completion = await openai.chat.completions.create({
+  //   model: "gpt-4o-mini",
+  //   messages: [
+  //     {
+  //       role: "system",
+  //       content: systemMessage,
+  //     },
+  //     {
+  //       role: "user",
+  //       content: userMessage,
+  //     },
+  //   ],
+  // });
 
-  const aiResponse = completion.choices[0].message.content;
+  // const aiResponse = completion.choices[0].message.content;
 
-  if (!aiResponse) {
-    throw new Error("Failed to generate AI response");
-  }
+  // if (!aiResponse) {
+  //   throw new Error("Failed to generate AI response");
+  // }
 
-  console.log("aiResponse", aiResponse);
+  // console.log("aiResponse", aiResponse);
 
   return {
-    position: aiResponse.match(/Job title: (.*)/)?.[1] || "",
-    company: aiResponse.match(/Company: (.*)/)?.[1] || "",
-    description: (aiResponse.match(/Description:([\s\S]*)/)?.[1] || "").trim(),
-    startDate: aiResponse.match(/Start date: (\d{4}-\d{2}-\d{2})/)?.[1],
-    endDate: aiResponse.match(/End date: (\d{4}-\d{2}-\d{2})/)?.[1],
+    // position: aiResponse.match(/Job title: (.*)/)?.[1] || "",
+    // company: aiResponse.match(/Company: (.*)/)?.[1] || "",
+    // description: (aiResponse.match(/Description:([\s\S]*)/)?.[1] || "").trim(),
+    // startDate: aiResponse.match(/Start date: (\d{4}-\d{2}-\d{2})/)?.[1],
+    // endDate: aiResponse.match(/End date: (\d{4}-\d{2}-\d{2})/)?.[1],
   } satisfies WorkExperience;
 }
